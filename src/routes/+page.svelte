648e9b21c53671026f929e894272f062ec61b083
huecester <jericho.paulino@yahoo.com.ph>
2023-06-07 21:28:36 -0700

    Hello, thing!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let thing = 'world';
+</script>
+
+<h1>Hello, {thing}!</h1>
+<input type="text" bind:value={thing} />
