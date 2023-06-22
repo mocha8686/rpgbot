8371001b3bd4d9f243c6bc75d9ba52e10d007709
huecester <jericho.paulino@yahoo.com.ph>
2023-06-21 22:31:15 -0700

    refactor: nuke everything and use classes

--- src/routes/fight/+page.svelte
+++ src/routes/fight/+page.svelte
@@ -1,251 +1,87 @@
 <script lang="ts">
-	import { z } from 'zod';
-
-	const COVER_MODIFIER = -0.4;
-	const FLANKING_MODIFIER = 0.6;
-
-	const cellSchema = z.enum(['player', 'wall', 'enemy', 'empty']);
-	const fieldSchema = cellSchema.array().array();
-
-	type Cell = z.infer<typeof cellSchema>;
-	type Field = z.infer<typeof fieldSchema>;
-	type Point = [number, number];
-	type Cover = {
-		north: boolean;
-		south: boolean;
-		east: boolean;
-		west: boolean;
-	};
-	type Unit = {
-		id: string;
-		type: Cell;
-		position: Point;
-	};
-
-	const mapData = [
-		'WWWWWWWWWW',
-		'W----WW--W',
-		'W--------W',
-		'W--W---W-W',
-		'W--W---W-W',
-		'W--W-----W',
-		'W-WWW--W-W',
-		'W-WWW----W',
-		'W--------W',
-		'WWWWWWWWWW',
+	const MAP = [
+		'---W---W--',
+		'----------',
+		'--WW--W---',
+		'--WW----W-',
+		'--WW----W-',
+		'----------',
+		'--W-------',
+		'--W-WWWW--',
+		'----------',
+		'---WWW----',
 	];
-	const map = fieldSchema.parse(mapData.map((row) => row.split('').map(parseToCell)));
 
-	const player: Unit = {
-		id: crypto.randomUUID(),
-		type: 'player',
-		position: [2, 2],
-	};
-	const enemy1: Unit = {
-		id: crypto.randomUUID(),
-		type: 'enemy',
-		position: [7, 7],
-	};
-	const enemy2: Unit = {
-		id: crypto.randomUUID(),
-		type: 'enemy',
-		position: [8, 4],
-	};
-	const enemies = [enemy1, enemy2];
+	type Cell = 'wall' | 'empty';
+	type UnitType = 'player' | 'enemy';
 
-	$: field = placeUnitsOnMap(structuredClone(map), [player, ...enemies]);
+	class Field {
+		map: Cell[][];
+		player: Unit;
+		enemies: Unit[];
 
-	function placeUnitsOnMap(map: Field, units: Unit[]): Field {
-		for (const unit of units) {
-			setCellAtPosition(map, unit.position, unit.type);
-		}
-		return map;
-	}
-
-	function calculateAimModifier(
-		field: Field,
-		playerPosition: Point,
-		enemyPosition: Point
-	): number {
-		const modifiers = getPossibleAimPositions(field, playerPosition, enemyPosition)
-			.filter((playerPosition) => canSee(field, playerPosition, enemyPosition))
-			.map((playerPosition) => {
-				const [coverModifier, flankingModifier] = calculateCoverModifiers(
-					field,
-					playerPosition,
-					enemyPosition
-				);
-
-				// TODO: Good angle
-
-				return coverModifier + flankingModifier;
-			});
-
-		if (modifiers.length === 0) return NaN;
-
-		return Math.max(...modifiers);
-	}
-
-	function getPossibleAimPositions(
-		field: Field,
-		attackerPosition: Point,
-		defenderPosition: Point
-	): Point[] {
-		const possiblePositions = [attackerPosition];
-		const attackerCover = calculateCover(field, attackerPosition);
-
-		if (
-			(attackerCover.north && defenderPosition[1] < attackerPosition[1]) ||
-			(attackerCover.south && defenderPosition[1] > attackerPosition[1])
-		) {
-			const westPos: Point = [attackerPosition[0] - 1, attackerPosition[1]];
-			if (getCellAtPosition(field, westPos) === 'empty') possiblePositions.push(westPos);
-
-			const eastPos: Point = [attackerPosition[0] + 1, attackerPosition[1]];
-			if (getCellAtPosition(field, eastPos) === 'empty') possiblePositions.push(eastPos);
+		constructor(size: number, player: Unit, enemies: Unit[]) {
+			this.map = new Array(size).fill(0).map((_) => new Array(size).fill(0));
+			this.player = player;
+			this.enemies = enemies;
 		}
 
-		if (
-			(attackerCover.east && defenderPosition[0] > attackerPosition[0]) ||
-			(attackerCover.west && defenderPosition[0] < attackerPosition[0])
-		) {
-			const northPos: Point = [attackerPosition[0], attackerPosition[1] - 1];
-			if (getCellAtPosition(field, northPos) === 'empty') possiblePositions.push(northPos);
-
-			const southPos: Point = [attackerPosition[0], attackerPosition[1] + 1];
-			if (getCellAtPosition(field, southPos) === 'empty') possiblePositions.push(southPos);
-		}
-
-		return possiblePositions;
-	}
-
-	function calculateCoverModifiers(
-		field: Field,
-		playerPosition: Point,
-		enemyPosition: Point
-	): [number, number] {
-		const cover = calculateCover(field, enemyPosition);
-
-		if (
-			(cover.north && playerPosition[1] < enemyPosition[1]) ||
-			(cover.south && playerPosition[1] > enemyPosition[1]) ||
-			(cover.east && playerPosition[0] > enemyPosition[0]) ||
-			(cover.west && playerPosition[0] < enemyPosition[0])
-		)
-			return [COVER_MODIFIER, 0];
-
-		return [0, FLANKING_MODIFIER];
-	}
-
-	function calculateCover(field: Field, position: Point): Cover {
-		return {
-			north: getCellAtPosition(field, [position[0], position[1] - 1]) === 'wall',
-			south: getCellAtPosition(field, [position[0], position[1] + 1]) === 'wall',
-			east: getCellAtPosition(field, [position[0] + 1, position[1]]) === 'wall',
-			west: getCellAtPosition(field, [position[0] - 1, position[1]]) === 'wall',
-		};
-	}
-
-	function canSee(field: Field, src: Point, dst: Point): boolean {
-		// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-		const dx = dst[0] - src[0];
-		const dy = dst[1] - src[1];
-		const steps = Math.max(Math.abs(dx), Math.abs(dy));
-
-		const xIncrement = dx / steps;
-		const yIncrement = dy / steps;
-
-		let x = src[0];
-		let y = src[1];
-
-		for (let i = 0; i < steps; i++) {
-			x += xIncrement;
-			y += yIncrement;
-
-			const cell = getCellAtPosition(field, [Math.round(x), Math.round(y)]);
-			if (cell === undefined || cell === 'wall') return false;
-		}
-
-		return true;
-	}
-
-	function getCellAtPosition(field: Field, position: Point): Cell | undefined {
-		return field.at(position[1])?.at(position[0]);
-	}
-
-	function setCellAtPosition(field: Field, position: Point, cell: Cell) {
-		field[position[1]][position[0]] = cell;
-	}
-
-	function parseToCell(input: string): Cell {
-		switch (input) {
-			case 'W':
-				return 'wall';
-			case '-':
-			default:
-				return 'empty';
+		static parse(map: string[], player: Unit, enemies: Unit[]): Field {
+			const field = new Field(map.length, player, enemies);
+			field.map = map.map((row) =>
+				row.split('').map((val) => {
+					switch (val) {
+						case 'W':
+							return 'wall';
+						case '-':
+						default:
+							return 'empty';
+					}
+				})
+			);
+			return field;
 		}
 	}
 
-	function cellToDisplayString(cell: Cell): string {
-		switch (cell) {
-			case 'player':
-				return '🧍';
-			case 'wall':
-				return '■';
-			case 'enemy':
-				return '👺';
-			case 'empty':
-				return '';
+	class Unit {
+		type: UnitType;
+		position: Point;
+
+		constructor(type: UnitType, position: Point) {
+			this.type = type;
+			this.position = position;
+		}
+
+		move(position: Point) {
+			this.position = position;
 		}
 	}
 
-	function updatePlayerPosition(newX: number, newY: number) {
-		if (getCellAtPosition(field, [newX, newY]) === 'empty') player.position = [newX, newY];
+	class Point {
+		x: number;
+		y: number;
+
+		constructor(x: number, y: number) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 
-	$: aimModifiers = enemies.map((enemy) =>
-		calculateAimModifier(field, player.position, enemy.position)
-	);
+	const player = new Unit('player', new Point(1, 1));
+	const enemy1 = new Unit('enemy', new Point(8, 8));
+	const enemy2 = new Unit('enemy', new Point(1, 6));
+	const field = Field.parse(MAP, player, [enemy1, enemy2]);
 </script>
 
 <h1>Fight</h1>
 
-<div
-	class="field"
-	style:grid-template-rows={`repeat(${mapData.length}, 2rem)`}
-	style:grid-template-columns={`repeat(${mapData[0].length}, 2rem)`}
->
-	{#each field as cols, y}
-		{#each cols as cell, x}
-			<button class:wall={cell === 'wall'} on:click={() => updatePlayerPosition(x, y)}
-				>{cellToDisplayString(cell)}</button
-			>
-		{/each}
-	{/each}
-</div>
-
-<p>Aim modifiers:</p>
-{#each aimModifiers as aimModifier}
-	<p>{`${(aimModifier * 100).toFixed(2)}%`}</p>
-{/each}
+<div class="field" />
 
 <style lang="scss">
 	.field {
 		display: grid;
 		width: fit-content;
-		grid-template-columns: repeat(10, 2rem);
-		grid-template-rows: repeat(10, 2rem);
 
 		border: 1px solid black;
-
-		& > * {
-			text-align: center;
-			border: 1px solid black;
-
-			&.wall {
-				background-color: black;
-			}
-		}
 	}
 </style>
